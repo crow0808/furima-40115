79b4d9eab3dcb292197c14edbf11d300967e87d9
crow0808 <tomoyanana@icloud.com>
2023-12-09 22:15:59 +0900

    配送料の負担の項目を設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,9 +5,11 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :items_status
+  belongs_to :shipping_cost
 
   with_options numericality: { other_than: 1, message: "can't be blank" } do
     validates :category_id
     validates :items_status_id
+    validates :shipping_cost
   end
 end
