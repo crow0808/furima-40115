c4c83fce4ddc5cedcf7f4d0a323b76676bd6f80a
crow0808 <tomoyanana@icloud.com>
2023-12-09 22:43:48 +0900

    発送までの日数の項目の設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,11 +7,13 @@ class Item < ApplicationRecord
   belongs_to :items_status
   belongs_to :shipping_cost
   belongs_to :prefecture
+  belongs_to :shipping_date
 
   with_options numericality: { other_than: 1, message: "can't be blank" } do
     validates :category_id
     validates :items_status_id
-    validates :shipping_cost
-    validates :prefecture
+    validates :shipping_cost_id
+    validates :prefecture_id
+    validates :shipping_date_id
   end
 end
