ec9a17b7cd457fb8ed783b326c108992ff6d7cfc
crow0808 <tomoyanana@icloud.com>
2023-12-09 22:33:03 +0900

    発送元の地域の項目の設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,10 +6,12 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :items_status
   belongs_to :shipping_cost
+  belongs_to :prefecture
 
   with_options numericality: { other_than: 1, message: "can't be blank" } do
     validates :category_id
     validates :items_status_id
     validates :shipping_cost
+    validates :prefecture
   end
 end
