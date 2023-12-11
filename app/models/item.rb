3fb0113482f032ad5045b8bd1c07abd11bcaf901
crow0808 <tomoyanana@icloud.com>
2023-12-11 14:54:55 +0900

    必要情報を入力し出品ボタンをせば情報がデータベースに保存される

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,14 +4,14 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
-  belongs_to :items_status
+  belongs_to :item_status
   belongs_to :shipping_cost
   belongs_to :prefecture
   belongs_to :shipping_date
 
   with_options numericality: { other_than: 1, message: "can't be blank" } do
     validates :category_id
-    validates :items_status_id
+    validates :item_status_id
     validates :shipping_cost_id
     validates :prefecture_id
     validates :shipping_date_id
@@ -22,7 +22,7 @@ class Item < ApplicationRecord
     validates :name
     validates :description
     validates :category_id
-    validates :items_status_id
+    validates :item_status_id
     validates :shipping_cost_id
     validates :prefecture_id
     validates :shipping_date_id
