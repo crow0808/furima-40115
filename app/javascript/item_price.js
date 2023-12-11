ddb8a9f2895b3ace3bd9590f060526b23dce0d34
crow0808 <tomoyanana@icloud.com>
2023-12-12 00:20:08 +0900

    テストコードを記述

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,11 +1,11 @@
-window.addEventListener('load', function(){
+window.addEventListener('load', () => {
   const price = document.getElementById("item-price");
   price.addEventListener("input",() => {
     const inputValue = price.value;
     const addTaxPrice = document.getElementById("add-tax-price");
     const profit = document.getElementById("profit");
     //販売手数料と販売利益を計算し表示する処理
-    addTaxPrice.textContent = Math.floor(inputValue * 0.1)
-    profit.textContent = Math.floor(inputValue - addTaxPrice.textContent)
+    addTaxPrice.textContent = Math.floor(inputValue * 0.1);
+    profit.textContent = inputValue - addTaxPrice.textContent;
   });
 });
