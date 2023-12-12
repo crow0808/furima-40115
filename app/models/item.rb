7514b3912b286191ce1f3306d980e15f9a41cd6f
crow0808 <tomoyanana@icloud.com>
2023-12-12 14:13:50 +0900

    priceカラムのバリデーションを修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -26,6 +26,6 @@ class Item < ApplicationRecord
     validates :shipping_cost_id
     validates :prefecture_id
     validates :shipping_date_id
-    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, format: { with: /\A\d+\z/ }
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, only_integer: true }
   end
 end
