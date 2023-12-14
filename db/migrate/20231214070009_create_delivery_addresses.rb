06041936209d3a9db3f3ef5325627b601ccaf008
crow0808 <tomoyanana@icloud.com>
2023-12-14 16:10:21 +0900

    ordersテーブルとdelivery_addressesテーブルを作成

--- db/migrate/20231214070009_create_delivery_addresses.rb
+++ db/migrate/20231214070009_create_delivery_addresses.rb
@@ -1,7 +1,13 @@
 class CreateDeliveryAddresses < ActiveRecord::Migration[7.0]
   def change
     create_table :delivery_addresses do |t|
-
+      t.references :order,          null: false, foreign_key: true
+      t.string     :postal_code,    null: false
+      t.integer    :prefecture_id,  null: false
+      t.string     :city,           null: false
+      t.string     :block,          null: false
+      t.string     :building_name
+      t.string     :phone_number,   null: false
       t.timestamps
     end
   end
