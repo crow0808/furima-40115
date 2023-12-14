2844ca064eb01fe3f6a1a8db17422384cb465dc2
crow0808 <tomoyanana@icloud.com>
2023-12-14 20:41:37 +0900

    決済処理

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :block, :building_name, :phone_number, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :city, :block, :building_name, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
     # delivery_addressモデルのバリデーション
